13b867e55689caf67eaeff52ea247981fcc36f0c
cssprasanth99 <cssprasanth99@gmail.com>
2024-01-19 13:49:11 +0530

    fixed bug in isPrime code

--- assignment2.js
+++ assignment2.js
@@ -1,15 +1,10 @@
 function isPrime(N) {
-    var count = 0;
-    for (var i = 2; i <= N / 2; i++) {
-        if (N % i === 0) {
-            count++;
-        }
-        if (count == 1) {
-            console.log(N, "is a prime number");
-        }
-        else {
-            console.log(N, "is not a prime");
+    for (var j = 2; j <= N / 2; j++) {
+        if (N % j === 0) {
+            console.log(N, "is not a prime number");
+            return;
         }
     }
+    console.log(N, "is a prime number");
 }
 isPrime(5);
