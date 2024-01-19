d9400c57bcf45449e53bb5312d346da86ac9bba8
MadgulaVamshi06 <vamshimadgula@gmail.com>
2024-01-19 13:35:22 +0530

    Corrected the isPrime logic

--- assignment2.js
+++ assignment2.js
@@ -2,14 +2,10 @@ function isPrime(N) {
     var count = 0;
     for (var i = 2; i <= N / 2; i++) {
         if (N % i === 0) {
-            count++;
-        }
-        if (count == 1) {
-            console.log(N, "is a prime number");
-        }
-        else {
-            console.log(N, "is not a prime");
+            console.log(N, "is not a prime number");
+            return;
         }
     }
+    console.log(N, "is a prime number");
 }
-isPrime(5);
+isPrime(17);
